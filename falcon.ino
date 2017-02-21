9e5c93b544c0f1a73b7a8fc750826924943cd77e
helium96 <masters2020@gmail.com>
2017-02-21 17:43:09 -0500

    Navigation

--- falcon.ino
+++ falcon.ino
@@ -1,134 +1,149 @@
 #include <SharpIR.h>
-
-//Project level constants 
-#define RANGE_SENSOR_PIN  A0
-
-//Wheel 1 Pins - Right
-const int WHEEL_ONE_PIN_ONE = 4; 
-const int WHEEL_ONE_PIN_TWO = 5; 
-const int WHEEL_ONE_PWM = 6; 
-
-//Wheel 2 Pins -Left
-const int WHEEL_TWO_PIN_ONE = 8; 
-const int WHEEL_TWO_PIN_TWO = 9; 
-const int WHEEL_TWO_PWM = 10; 
-
-//Wheel Speed 
-int WHEEL_SPEED = 140; 
-
-int state;
-
-
-SharpIR rangeSensor(RANGE_SENSOR_PIN, 25, 93, 20150);
-
-int distance; 
-
-void setup(){
-
-	pinMode( RANGE_SENSOR_PIN, INPUT);
-  //Wheel 1 
-  pinMode(WHEEL_ONE_PIN_ONE, OUTPUT); 
-  pinMode(WHEEL_ONE_PIN_TWO, OUTPUT); 
-  pinMode(WHEEL_ONE_PWM, OUTPUT); 
-
+#define sensor A0 // Sharp IR GP2Y0A41SK0F (4-30cm, analog)
  
-  //Wheel 2 
-  pinMode(WHEEL_TWO_PIN_ONE, OUTPUT); 
+//Wheel 1 Pins - Right
+const int WHEEL_ONE_PIN_ONE = 4;
+const int WHEEL_ONE_PIN_TWO = 5;
+const int WHEEL_ONE_PWM = 6;
+ 
+//Wheel 2 Pins -Left
+const int WHEEL_TWO_PIN_ONE = 8;
+const int WHEEL_TWO_PIN_TWO = 9;
+const int WHEEL_TWO_PWM = 10;
+ 
+//Wheel Speed
+int WHEEL_SPEED = 140;
+ 
+int state;
+ 
+ 
+#define ir A0
+#define model 20150
+ 
+ 
+int dis;
+void setup(){
+ 
+ 
+  //Wheel 1
+  pinMode(WHEEL_ONE_PIN_ONE, OUTPUT);
+  pinMode(WHEEL_ONE_PIN_TWO, OUTPUT);
+  pinMode(WHEEL_ONE_PWM, OUTPUT);
+ 
+ 
+  //Wheel 2
+  pinMode(WHEEL_TWO_PIN_ONE, OUTPUT);
   pinMode(WHEEL_TWO_PIN_TWO, OUTPUT);
-  pinMode(WHEEL_TWO_PWM, OUTPUT); 
-
+  pinMode(WHEEL_TWO_PWM, OUTPUT);
+ 
   Serial.begin(9600);
 }
-
-
-
+ 
+ 
+ 
 void loop(){
-	distance = rangeSensor.distance();
-	if (distance < 10)
-	{
-		Stop();
-		delay(2000);
-		Right(100);
-		delay(3000);
-	}
-	else{
-		Forward(140);
-	}
+  dis = IR();
+ // Serial.println(distance);
+  if (dis < 20)
+  {
+    Stop();
+    //delay(2000);
+    Reverse(120);
+    delay(500);
+    Right(100);
+    delay(250);
+  }
+  else{
+    Forward(90);
+  }
 }
-
+ 
 void Reverse(int WHEEL_SPEED)
 {
-  //Wheel 1 
-  digitalWrite(WHEEL_ONE_PIN_ONE, LOW); 
-  digitalWrite(WHEEL_ONE_PIN_TWO, HIGH); 
-
-  analogWrite(WHEEL_ONE_PWM, WHEEL_SPEED); 
+  //Wheel 1
+  digitalWrite(WHEEL_ONE_PIN_ONE, LOW);
+  digitalWrite(WHEEL_ONE_PIN_TWO, HIGH);
  
-  //Wheel 2 
-  digitalWrite(WHEEL_TWO_PIN_ONE, HIGH); 
-  digitalWrite(WHEEL_TWO_PIN_TWO, LOW); 
-
-  analogWrite(WHEEL_TWO_PWM, WHEEL_SPEED); 
+  analogWrite(WHEEL_ONE_PWM, WHEEL_SPEED);
+ 
+  //Wheel 2
+  digitalWrite(WHEEL_TWO_PIN_ONE, HIGH);
+  digitalWrite(WHEEL_TWO_PIN_TWO, LOW);
+ 
+  analogWrite(WHEEL_TWO_PWM, WHEEL_SPEED);
 }
-
+ 
 void Forward(int WHEEL_SPEED)
 {
-  //Wheel 1 
-  digitalWrite(WHEEL_ONE_PIN_ONE, HIGH); 
-  digitalWrite(WHEEL_ONE_PIN_TWO, LOW); 
-
-  analogWrite(WHEEL_ONE_PWM, WHEEL_SPEED); 
+  //Wheel 1
+  digitalWrite(WHEEL_ONE_PIN_ONE, HIGH);
+  digitalWrite(WHEEL_ONE_PIN_TWO, LOW);
  
-  //Wheel 2 
-  digitalWrite(WHEEL_TWO_PIN_ONE, LOW); 
-  digitalWrite(WHEEL_TWO_PIN_TWO, HIGH); 
-
-  analogWrite(WHEEL_TWO_PWM, WHEEL_SPEED); 
+  analogWrite(WHEEL_ONE_PWM, WHEEL_SPEED);
+ 
+  //Wheel 2
+  digitalWrite(WHEEL_TWO_PIN_ONE, LOW);
+  digitalWrite(WHEEL_TWO_PIN_TWO, HIGH);
+ 
+  analogWrite(WHEEL_TWO_PWM, WHEEL_SPEED);
 }
-
+ 
 void Stop()
 {
-  //Wheel 1 
-  digitalWrite(WHEEL_ONE_PIN_ONE, LOW); 
-  digitalWrite(WHEEL_ONE_PIN_TWO, LOW); 
-  
-  analogWrite(WHEEL_ONE_PWM, 0); 
+  //Wheel 1
+  digitalWrite(WHEEL_ONE_PIN_ONE, LOW);
+  digitalWrite(WHEEL_ONE_PIN_TWO, LOW);
  
-  //Wheel 2 
-  digitalWrite(WHEEL_TWO_PIN_ONE, LOW); 
-  digitalWrite(WHEEL_TWO_PIN_TWO, LOW); 
-
-  analogWrite(WHEEL_TWO_PWM, 0); 
+  analogWrite(WHEEL_ONE_PWM, 0);
+ 
+  //Wheel 2
+  digitalWrite(WHEEL_TWO_PIN_ONE, LOW);
+  digitalWrite(WHEEL_TWO_PIN_TWO, LOW);
+ 
+  analogWrite(WHEEL_TWO_PWM, 0);
 }
-
+ 
 void Right(int WHEEL_SPEED)
 {
-  //Wheel 1 
-  digitalWrite(WHEEL_ONE_PIN_ONE, LOW); 
-  digitalWrite(WHEEL_ONE_PIN_TWO, HIGH); 
-  
-  analogWrite(WHEEL_ONE_PWM, WHEEL_SPEED); 
-
+  //Wheel 1
+  digitalWrite(WHEEL_ONE_PIN_ONE, LOW);
+  digitalWrite(WHEEL_ONE_PIN_TWO, HIGH);
+ 
+  analogWrite(WHEEL_ONE_PWM, WHEEL_SPEED);
+ 
   //Wheel 2
-  digitalWrite(WHEEL_TWO_PIN_ONE, LOW); 
-  digitalWrite(WHEEL_TWO_PIN_TWO, HIGH); 
-
-  analogWrite(WHEEL_TWO_PWM, WHEEL_SPEED); 
+  digitalWrite(WHEEL_TWO_PIN_ONE, LOW);
+  digitalWrite(WHEEL_TWO_PIN_TWO, HIGH);
+ 
+  analogWrite(WHEEL_TWO_PWM, WHEEL_SPEED);
 }
-
+ 
 void Left(int WHEEL_SPEED)
 {
-  //Wheel 1 
-  digitalWrite(WHEEL_ONE_PIN_ONE, HIGH); 
-  digitalWrite(WHEEL_ONE_PIN_TWO, LOW); 
-  
-  analogWrite(WHEEL_ONE_PWM, WHEEL_SPEED); 
-
+  //Wheel 1
+  digitalWrite(WHEEL_ONE_PIN_ONE, HIGH);
+  digitalWrite(WHEEL_ONE_PIN_TWO, LOW);
+ 
+  analogWrite(WHEEL_ONE_PWM, WHEEL_SPEED);
+ 
   //Wheel 2
-  digitalWrite(WHEEL_TWO_PIN_ONE, HIGH); 
-  digitalWrite(WHEEL_TWO_PIN_TWO, LOW); 
-
-  analogWrite(WHEEL_TWO_PWM, WHEEL_SPEED); 
+  digitalWrite(WHEEL_TWO_PIN_ONE, HIGH);
+  digitalWrite(WHEEL_TWO_PIN_TWO, LOW);
+ 
+  analogWrite(WHEEL_TWO_PWM, WHEEL_SPEED);
 }
-
-
+ 
+ 
+int IR(){
+   // 5v
+  float volts = analogRead(sensor)*0.0048828125;  // value from sensor * (5/1024)
+  int distance = 13*pow(volts, -1); // worked out from datasheet graph
+  Serial.println(distance);
+  delay(100); // slow down serial port
+ 
+  if (distance <= 30){
+    Serial.println(distance);   // print the distance
+  }
+  return distance;
+ 
+  }
